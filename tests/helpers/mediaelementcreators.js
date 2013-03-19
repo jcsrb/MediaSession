2cfd89b3d1f2a8cf8bd788cacf5a4e92ac854440
Jakob Cosoroabă <cosoroaba@gmail.com>
2013-03-19 02:15:35 +0200

    a bit of linting

--- tests/helpers/mediaelementcreators.js
+++ tests/helpers/mediaelementcreators.js
@@ -1,47 +1,45 @@
-function createVideo(wrapperId, autorestore){
+function createVideo(wrapperId, autorestore) {
   return createMedia(wrapperId, "video", demoVideoSrc, autorestore);
 }
 
-function createAudio(wrapperId, autorestore){  
+function createAudio(wrapperId, autorestore) {
   return createMedia(wrapperId, "audio", demoAudioSrc, autorestore);
-} 
+}
 
-function createMedia(wrapperId, elementTagName, sources, autorestore){
-  var autorestore = !!autorestore;
-  
+function mapSources (sources, sourceElements) {
+  var count = sourceElements.length;
+  while (count > 0) {
+    count -= 1;
+    sourceElements[count].setAttribute("src", sources[count].src);
+    sourceElements[count].setAttribute("type", sources[count].mime);
+  }
+}
+
+function createMedia(wrapperId, elementTagName, sources, autorestore) {
+  autorestore = !!autorestore;
   var structure = DOM.init(
-    DOM.el('div#'+wrapperId).content(
+    DOM.el('div#' + wrapperId).content(
       DOM.el(elementTagName)
-    )              
-  );
-
-
-  var mediaElement = structure.querySelector(elementTagName);
+    )
+  ),
+    mediaElement = structure.querySelector(elementTagName),
+    sourcesNodes = [],
+    count = sources.length;
   mediaElement.setAttribute("controls", '');
   mediaElement.setAttribute("session", '');
-  if(autorestore){
+
+  if (autorestore) {
     mediaElement.setAttribute("autorestore", '');
   }
 
-  var sourcesNodes = [];
-  var count = sources.length;  
   while (count > 0) {
-      count -= 1;      
-      mediaElement.appendChild(DOM.init(DOM.el('source')));                
+    count -= 1;
+    mediaElement.appendChild(DOM.init(DOM.el('source')));
   }
-     
+
   var sourceElements = structure.querySelectorAll("source");
-  mapSources(sources,sourceElements);
+  mapSources(sources, sourceElements);
 
-  
-  return structure; 
+  return structure;
 }
 
-function mapSources(sources, sourceElements){  
-  var count = sourceElements.length;  
-  while (count > 0) {
-      count -= 1;                            
-      sourceElements[count].setAttribute("src", sources[count].src);
-      sourceElements[count].setAttribute("type", sources[count].mime);            
-  }
-}
